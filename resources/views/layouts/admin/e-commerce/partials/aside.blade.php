e0641a7225e26e65bbe9e5aad62d3491da708a7c
Sahadat Hossen <freelancershahadat4@gmail.com>
2024-01-14 18:11:12 +0600

    dashboard color retrive

--- resources/views/layouts/admin/e-commerce/partials/aside.blade.php
+++ resources/views/layouts/admin/e-commerce/partials/aside.blade.php
@@ -5,7 +5,7 @@
         font-size: 15px;
     }
 </style>
-<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background:#28a745">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background:#096629">
     <!-- Brand Logo -->
     <a href="{{route('admin.dashboard')}}" class="brand-link">
         <img src="/uploads/setting/{{setting('logo')}}" alt="Logo" class="brand-image"
